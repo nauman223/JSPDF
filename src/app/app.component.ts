af48e8c8af95fde0e7a068109a4138a14fa90e12
nauman223 <muhammadnauman223@gmail.com>
2021-12-02 13:49:51 +0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component, VERSION } from '@angular/core';
-import { jsPDF } from "jspdf";
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  VERSION,
+} from '@angular/core';
+import { jsPDF } from 'jspdf';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-   
+export class AppComponent implements OnInit, OnChanges {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+
+  exportpdf = () => {
+    const doc = new jsPDF({
+      orientation: 'landscape',
+      unit: 'in',
+      format: [4, 2],
+    });
+
+    doc.text('Hello world!', 1, 1);
+    doc.save('two-by-four.pdf');
+  };
 }
